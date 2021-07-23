24d7354b45a8824b68035183c1de1b8213ef962b
Patrick Samson <79001601+patricklsamson@users.noreply.github.com>
2021-07-23 19:04:03 +0800

    Update article

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,10 +1,23 @@
 class Article < ApplicationRecord
+  extend FriendlyId
+  friendly_id :title, use: :slugged
+
+  scope :most_recent, -> { order(id: :desc) }
+
+  def should_generate_new_friendly_id?
+    title_changed?
+  end
+
+  def publish_time
+    "Published #{created_at.strftime('%I:%M %p')} #{created_at.strftime('%b %-d, %Y')}"
+  end
+
   include Visible
 
   has_many :comments, dependent: :destroy
 
-  validates :name, presence: true,
-                   uniqueness: true
+  validates :title, presence: true,
+                    uniqueness: true
 
   validates :body, presence: true,
                    length: { minimum: 10 }
