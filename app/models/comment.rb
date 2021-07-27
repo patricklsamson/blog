ca49afcbd95e5d80623886f56c8b1d04d50d89ce
Patrick Samson <79001601+patricklsamson@users.noreply.github.com>
2021-07-27 21:02:46 +0800

    Update comment

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,4 +1,8 @@
 class Comment < ApplicationRecord
+  def timestamp
+    "Published #{created_at.strftime('%I:%M %p')} #{created_at.strftime('%b %-d, %Y')}"
+  end
+
   include Visible
   belongs_to :article
 end
