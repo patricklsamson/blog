cd69df6651c43b1a7f1595c71a51c65056d82467
Patrick Samson <79001601+patricklsamson@users.noreply.github.com>
2021-07-31 23:57:04 +0800

    Update schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_07_22_162454) do
+ActiveRecord::Schema.define(version: 2021_07_31_153344) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -32,7 +32,6 @@ ActiveRecord::Schema.define(version: 2021_07_22_162454) do
     t.bigint "article_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.string "status"
     t.index ["article_id"], name: "index_comments_on_article_id"
   end
 
