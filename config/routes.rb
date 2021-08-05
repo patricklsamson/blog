da812eac0b35e3d16bd8968f3879973e8888afa2
Patrick Samson <79001601+patricklsamson@users.noreply.github.com>
2021-08-05 10:11:46 +0800

    Update routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
   get 'archives' => 'pages#archives', as: :archives
   get 'about' => 'pages#about', as: :about
   get 'contact' => 'pages#contact', as: :contact
